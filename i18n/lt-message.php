f93c10380422d8de2d618424036f3e1c5979423f
Edmondas Girkantas <edmondas@datalogistiikka.fi>
2012-12-11 12:35:12 +0200

    Added lithuanian translation.

---
+++ i18n/lt-message.php
@@ -0,0 +1,34 @@
+<?php
+
+    /**
+     * Lithuanian language file for gallery plugin
+     *
+     * @package wolf
+     * @subpackage plugin.fancy_image_gallery.translations
+     *
+     * @author Edmondas Girkantas <edmondas@datalogistiikka.fi>
+     * @version Wolf 0.6.0 + above
+     */
+
+    return array(
+    'Documentation' => 'Dokumentacija',
+	'Settings' => 'Nustatymai',
+	'Create Thumbnails - All fields are mandatory' => 'Sukurti miniatiūras - privaloma užpildyti visus laukus',
+	'Gallery Path' => 'Kelias iki galerijos',
+	'Specify name of the folder under "public/images/" with a trailing slash. Example: mygallery/' =>
+	'Nurodykite aplanko esančio "public/images/" kartu su pasviruoju brūkšniu pabaigoje, pvz.: manogalerija/',
+	'Thumbnail Width' => 'Miniatiūros plotis',
+	'Specify thumbnail width in pixels' => 'Nurodykite miniatiūros plotį pikseliais',
+	'Thumbnail Height' => 'Miniatriūros aukštis',
+	'Specify thumbnail height in pixels' => 'Nurodykite miniatūros aukštį pikseliais',
+	'Create Thumbnails' => 'Sukurti miniatiūras',
+	'Thumbnails are successfully created!' => 'Miniatiūros buvo sėkmingai sukurtos.',
+	'There are no images in this folder!' => 'Šiame kataloge nėra atvaizdų!',
+	'Provides easy to use image gallery with fancybox effect.' =>
+	'Lengvai panaudojama atvaizdų galerija su madingos dėžutės efektu.',
+	'Put the trailing slash at the end of the folder name!' => 'Padėkite pasvirąjį brūkšnį aplanko vardo pabaigoje!',
+	'The folder does not exist!' => 'Aplankas neegzistuoja!',
+	'Specify thumbnail width!' => 'Nurodykite miniatiūros plotį!',
+	'Specify thumbnail height!' => 'Nurodykite miniatiūros aukštį!',
+	'There are no images in this gallery.' => 'Šioje galerijoje nėra atvaizdų.'
+    );
